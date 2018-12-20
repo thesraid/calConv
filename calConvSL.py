5f8f3b0aabbcc2e54ca9b2818f4f50dc5afc3dc7
root <thesraid@gmail.com>
2018-12-20 18:01:51 +0100

    Well formatted entries

--- calConvSL.py
+++ calConvSL.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """
 thesraid@gmail.com
@@ -11,6 +12,7 @@ import argparse # Used to read in arguments from the command line
 import re # Used to write regular expressions to match patterns in the ics file
 import dateutil.parser # Used to convert the date format
 import datetime # Used to extract the date and time seperately from the date
+from subprocess import call # Used to run Operating SYstem commands
 ###########################################################################################
 """
 Get command line args from the user.
@@ -43,68 +45,53 @@ def main():
 
    # Get the arguments from the command line using the get_args function above
    args = get_args()
+  
+   # Run dos2unix to get rid of windows crap
+   call(["dos2unix", "-q", args.ics])
 
    # Open the ics file, read it's contents to ics then close the file
    icsFile = open(args.ics, 'r')
    ics = icsFile.read().strip()
    icsFile.close()
-   
-   # Need to clean the file first to get rid of messy charecters, multi-line entries
-   # Cut up the file into smaller chunks
-   # Regex each file
 
-   # Then big regex with loads of groups goes here.
+   csvFile = open(args.csv, 'w+')
+   csvFile.write("Event Venue Name,Event Organizer Name,Event Name,Event Start Date,Event Start Time,Event End Date,Event Description,Timezone,tag\n")
 
-   # BEGIN:VEVENT\sDTSTAMP(.*)\sLAST-MODIFIED:(.*)\sCREATED:(.*)\sSEQUENCE:(.*)\sORGANIZER;CN=(.*):MAILTO(.*)\sDTSTART:(.*)\sDTEND:(.*)\sUID:(.*)\sSUMMARY:(.*)\sLOCATION(.*[\S\s]*?(?=))\sURL:(.*)\sDESCRIPTION:(.*[\s\S]*?(?=))CLASS:(.*)\sSTATUS:(.*)\sPARTSTAT:(.*)\sEND:VEVENT
 
-   reg = re.compile(r'BEGIN:VEVENT.*DTSTAMP:(.*)LAST-MODIFIED:(.*)CREATED:(.*)SEQUENCE:(.*)ORGANIZER;CN=(.*):MAILTO(.*)DTSTART:(.*)DTEND:(.*)UID:(.*)SUMMARY:(.*)LOCATION:(.*?(?=))URL:(.*)DESCRIPTION:(.*?(?=))CLASS:(.*)STATUS:(.*)PARTSTAT:(.*)END:VEVENT', re.DOTALL|re.MULTILINE)
-   for out in re.finditer(reg, ics):
-      print out.group(1).replace('\n','')
-      print out.group(2).replace('\n','')
-      print out.group(3).replace('\n','')
-      print out.group(4).replace('\n','')
-      print out.group(5).replace('\n','')
-      print out.group(6).replace('\n','')
-      print out.group(7).replace('\n','')
-      print out.group(8).replace('\n','')
-      print out.group(9).replace('\n','')
-      print out.group(10).replace('\n','')
-      print out.group(11).replace('\n','')
-      print out.group(12).replace('\n','')
-      print (((out.group(13).replace('\n','')).replace('\\n',' ')).replace('\r ','')).replace('\\','')
-      print out.group(14).replace('\n','')
-      print out.group(15).replace('\n','')
-      print out.group(16).replace('\n','')
+   reg = re.compile(r'BEGIN:VEVENT(.*?(?=))END:VEVENT', re.DOTALL|re.MULTILINE)
+   for block in re.finditer(reg, ics):
+      reg = re.compile(r'DTSTAMP:(.*)LAST-MODIFIED:(.*)CREATED:(.*)SEQUENCE:(.*)ORGANIZER;CN=(.*):MAILTO(.*)DTSTART:(.*)DTEND:(.*)UID:(.*)SUMMARY:(.*)LOCATION:(.*?(?=))URL:(.*)DESCRIPTION:(.*?(?=))CLASS:(.*)STATUS:(.*)PARTSTAT:(.*)', re.DOTALL|re.MULTILINE)
+      for out in re.finditer(reg, block.group(1)):
+         '''
+         print out.group(1).replace('\n','') # DTSTAMP
+         print out.group(2).replace('\n','') # LAST-MODIFIED:
+         print out.group(3).replace('\n','') # CREATED:
+         print out.group(4).replace('\n','') # SEQUENCE:
+         print out.group(5).replace('\n','') # ORGANIZER
+         print out.group(6).replace('\n','') # MAILTO
+         print out.group(7).replace('\n','') # DTSTART
+         print out.group(8).replace('\n','') # DTEND
+         print out.group(9).replace('\n','') # UID
+         print out.group(10).replace('\n','') # SUMMARY
+         print out.group(11).replace('\n','') # LOCATION
+         print out.group(12).replace('\n','') # URL
+         print (((out.group(13).replace('\n','')).replace('\\n','')).replace('\r ','')).replace('\\','') # DESCRIPTION
+         print out.group(14).replace('\n','') # CLASS
+         print out.group(15).replace('\n','') # STATUS
+         print out.group(16).replace('\n','') # PARTSTAT
+         '''
+         csvFile.write((out.group(11).replace('\n','').replace(',', ''))  + ',') # LOCATION
+         csvFile.write((out.group(5).replace('\n','').replace(',', '')) + ',') # ORGANIZER
+         csvFile.write((out.group(10).replace('\n','').replace(',', '')) + ',') # SUMMARY
+         csvFile.write((out.group(7).replace('\n','').replace(',', '')) + ',') # DTSTART
+         csvFile.write((out.group(7).replace('\n','').replace(',', '')) + ',') # DTSTARTTIME
+         csvFile.write((out.group(8).replace('\n','').replace(',', '')) + ',') # DTEND
+         csvFile.write((((out.group(13).replace('\n ','').replace(',', ''))).replace('\\n','')).replace('\\','') + ',') # DESCRIPTION
+         csvFile.write(",Europe/Dublin,Ireland\n")
 
-   '''
-   out = re.search(r'BEGIN:VEVENT.*DTSTAMP:(.*)LAST-MODIFIED:(.*)CREATED:(.*)SEQUENCE:(.*)ORGANIZER;CN=(.*):MAILTO(.*)DTSTART:(.*)DTEND:(.*)UID:(.*)SUMMARY:(.*)LOCATION:(.*?(?=))URL:(.*)DESCRIPTION:(.*?(?=))CLASS:(.*)STATUS:(.*)PARTSTAT:(.*)END:VEVENT', ics, re.DOTALL|re.MULTILINE)
-   print out.group(1)
-   print out.group(2)
-   print out.group(3)
-   print out.group(4)
-   print out.group(5)
-   print out.group(6)
-   print out.group(7)
-   print out.group(8)
-   print out.group(9)
-   print out.group(10)
-   print out.group(11)
-   print out.group(12)
-   print out.group(13)
-   print out.group(14)
-   print out.group(15)
-   print out.group(16)
-   '''
+   csvFile.close()
 
-   # Used regex to search through the file to match the follow patterns. Then save the result
-   # Everything in the brackets (.*) represents text that we want to save. It will automatically be saved as group 1
-   # TO see the result you need to run print result.group(1) to show just the data in group 1
-   #description = re.search(r'DESCRIPTION:(.*)^CLASS', ics, re.DOTALL|re.MULTILINE)
-   #descOneLine = description.group(1).replace('\n','')
-   #descNewLine = descOneLine.replace('\\n',' ')
-   #print(descNewLine.replace('\r ',''))
-
-   
+         
 ###########################################################################################
 
 """ Start program """
