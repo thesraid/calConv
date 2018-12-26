a59314e98dd2a49240147e7646fbaaec3f7030d7
root <thesraid@gmail.com>
2018-12-26 13:53:54 +0100

    Working

--- calConv.py
+++ calConv.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """
 thesraid@gmail.com
-
-0.1 Switches, Open File, regex out lines and convert ISO 8601 date
-0.2 write to CSV in correct format
 """
 
 import argparse # Used to read in arguments from the command line
 import re # Used to write regular expressions to match patterns in the ics file
 import dateutil.parser # Used to convert the date format
 import datetime # Used to extract the date and time seperately from the date
+from subprocess import call # Used to run Operating SYstem commands
 ###########################################################################################
 """
 Get command line args from the user.
@@ -43,64 +42,65 @@ def main():
 
    # Get the arguments from the command line using the get_args function above
    args = get_args()
+  
+   # Run dos2unix to get rid of windows crap
+   call(["dos2unix", "-q", args.ics])
 
    # Open the ics file, read it's contents to ics then close the file
    icsFile = open(args.ics, 'r')
    ics = icsFile.read().strip()
    icsFile.close()
 
-   # Used regex to search through the file to match the follow patterns. Then save the result
-   # Everything in the brackets (.*) represents text that we want to save. It will automatically be saved as group 1
-   # TO see the result you need to run print result.group(1) to show just the data in group 1
-   description = re.search(r'DESCRIPTION:(.*)^CLASS', ics, re.DOTALL|re.MULTILINE)
-   location = re.search(r'LOCATION:(.*)', ics)
-   organiser = re.search(r'ORGANIZER;CN=(.*):MAILTO', ics)
-   dtstart = re.search(r'DTSTART:(.*)', ics)
-   dtend = re.search(r'DTEND:(.*)', ics)
-   summary = re.search(r'SUMMARY:(.*)', ics)
-
-   # Convert the date to standard date
-   start = dateutil.parser.parse(dtstart.group(1))
-   end = dateutil.parser.parse(dtend.group(1))
-
-   # Extract the date and time from the dates
-   startDate = start.strftime('%Y-%m-%d')
-   endDate = end.strftime('%Y-%m-%d')
-   startTime = start.strftime('%H:%M:%S')
-   endTime = end.strftime('%H:%M:%S')
-
-   # Write the above data to a CSV file using the approprate column names
-   # https://theeventscalendar.com/knowledgebase/csv-files-options-and-examples
-
-   # Open the csv file, write to it then close the file
-   # group(1) is used for the regex results to write only the data in group 1 (.*)
-   # The replace option removes stupid winodws return chars
    csvFile = open(args.csv, 'w+')
-   csvFile.write("Event Venue Name,Event Organizer Name,Event Name,Event Start Date,Event Start Time,Event End Date,Event Description,Timezone,tag\n")
-   # This should be in a loop reading from a matrix or something maybe
-   csvFile.write(location.group(1).replace('\r',''))
-   csvFile.write(",")
-   csvFile.write(organiser.group(1).replace('\r',''))
-   csvFile.write(",")
-   csvFile.write(summary.group(1).replace('\r',''))
-   csvFile.write(",")
-   csvFile.write(startDate.replace('\r',''))
-   csvFile.write(",")
-   csvFile.write(startTime.replace('\r',''))
-   csvFile.write(",")
-   csvFile.write(endDate.replace('\r',''))
-   csvFile.write(",")
-   # Need to do some work to nicelt format the description date
-   descOneLine = description.group(1).replace('\n','')
-   descNewLine = descOneLine.replace('\\n',' ')
-   csvFile.write(descNewLine.replace('\r ',''))
-   csvFile.write(",Europe/Dublin,Ireland\n")
-   # Need to add this stuff to pad out the end as the import on the site is buggy
-   
-   csvFile.close()
- 
+   csvFile.write("Event Venue Name,Event Organizer Name,Event Name,Event Start Date,Event Start Time,Event End Date,Event Description,Timezone\n")
 
-   
+
+   reg = re.compile(r'BEGIN:VEVENT(.*?(?=))END:VEVENT', re.DOTALL|re.MULTILINE)
+   for block in re.finditer(reg, ics):
+      reg = re.compile(r'DTSTAMP:(.*)LAST-MODIFIED:(.*)CREATED:(.*)SEQUENCE:(.*)ORGANIZER;CN=(.*):MAILTO(.*)DTSTART:(.*)DTEND:(.*)UID:(.*)SUMMARY:(.*)LOCATION:(.*?(?=))URL:(.*)DESCRIPTION:(.*?(?=))CLASS:(.*)STATUS:(.*)PARTSTAT:(.*)', re.DOTALL|re.MULTILINE)
+      for out in re.finditer(reg, block.group(1)):
+         '''
+         print out.group(1).replace('\n','') # DTSTAMP
+         print out.group(2).replace('\n','') # LAST-MODIFIED:
+         print out.group(3).replace('\n','') # CREATED:
+         print out.group(4).replace('\n','') # SEQUENCE:
+         print out.group(5).replace('\n','') # ORGANIZER
+         print out.group(6).replace('\n','') # MAILTO
+         print out.group(7).replace('\n','') # DTSTART
+         print out.group(8).replace('\n','') # DTEND
+         print out.group(9).replace('\n','') # UID
+         print out.group(10).replace('\n','') # SUMMARY
+         print out.group(11).replace('\n','') # LOCATION
+         print out.group(12).replace('\n','') # URL
+         print (((out.group(13).replace('\n','')).replace('\\n','')).replace('\r ','')).replace('\\','') # DESCRIPTION
+         print out.group(14).replace('\n','') # CLASS
+         print out.group(15).replace('\n','') # STATUS
+         print out.group(16).replace('\n','') # PARTSTAT
+         '''
+
+
+         # Convert the date to standard date
+         start = dateutil.parser.parse(out.group(7))
+         end = dateutil.parser.parse(out.group(8))
+
+         # Extract the date and time from the dates
+         startDate = start.strftime('%Y-%m-%d')
+         endDate = end.strftime('%Y-%m-%d')
+         startTime = start.strftime('%H:%M:%S')
+         endTime = end.strftime('%H:%M:%S')
+
+         csvFile.write(((((out.group(11).replace('\n ','').replace(',', ''))).replace('\\n',' ')).replace('\\','')).replace('\n','') + ',') # LOCATION
+         csvFile.write(((((out.group(5).replace('\n ','').replace(',', ''))).replace('\\n',' ')).replace('\\','')).replace('\n','') + ',') # ORGANIZER
+         csvFile.write(((((out.group(10).replace('\n ','').replace(',', ''))).replace('\\n',' ')).replace('\\','')).replace('\n','') + ',') # SUMMARY
+         csvFile.write(startDate + ',') # DTSTART
+         csvFile.write(startTime + ',') # DTSTARTTIME
+         csvFile.write(endDate + ',') # DTEND
+         csvFile.write(((((out.group(13).replace('\n ','').replace(',', ''))).replace('\\n',' ')).replace('\\','')).replace('\n','') + ',') # DESCRIPTION
+         csvFile.write("Europe/Dublin\n")
+
+   csvFile.close()
+
+         
 ###########################################################################################
 
 """ Start program """
